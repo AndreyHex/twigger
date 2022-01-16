e363a7cc8b6512f3bb44f40bb07713b447f9461a
hex <me@example.com>
2022-01-16 22:03:46 +0300

    tests

--- backend/src/main/java/com/twigger/config/DataSourceConfig.java
+++ backend/src/main/java/com/twigger/config/DataSourceConfig.java
@@ -3,6 +3,7 @@ package com.twigger.config;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 import java.net.URI;
@@ -12,9 +13,10 @@ import java.net.URISyntaxException;
 public class DataSourceConfig {
 
     @Bean
+    @Profile("dev")
     public DataSource getDataSource() throws URISyntaxException {
 
-        URI dbUri = new URI( "mysql://twigger_app:twigger@127.0.0.1/twigger_db");// System.getenv("CLEARDB_DATABASE_URL"));
+        URI dbUri = new URI( "mysql://twigger_app:twigger@127.0.0.1/twigger_db");// System.getenv("DATABASE_URL"));
 
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
@@ -25,6 +27,17 @@ public class DataSourceConfig {
         dataSourceBuilder.url(dbUrl);
         dataSourceBuilder.username(username);
         dataSourceBuilder.password(password);
+        return dataSourceBuilder.build();
+    }
+
+    @Bean
+    @Profile("test")
+    public DataSource getTestDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:jpa_jbd;DB_CLOSE_DELAY=-1");
+        dataSourceBuilder.username("sa");
+        dataSourceBuilder.password("sa");
         return dataSourceBuilder.build();
     }
 
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO role (id, name) VALUE (1, 'ROLE_ADMIN');
-INSERT INTO role (id, name) VALUE (2, 'ROLE_USER');
+INSERT INTO role (id, name) VALUES (1, 'ROLE_ADMIN');
+INSERT INTO role (id, name) VALUES (2, 'ROLE_USER');
